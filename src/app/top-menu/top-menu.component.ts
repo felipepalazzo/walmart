8d78c65f642775e0450caa65f325ad4ce8cd5c58
Felipe Palazzo <palazzofelipe@gmail.com>
2017-07-09 10:51:19 -0300

    Add orders amount to cart

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { CartService } from '../cart/cart.service';
+import { Product } from '../models/product.interface';
 
 @Component({
   selector: 'top-menu',
   templateUrl: './top-menu.component.html',
   styleUrls: ['./top-menu.component.scss']
 })
-export class TopMenuComponent {}
+export class TopMenuComponent {
+  constructor(private cartService: CartService) { }
+  orders: Product[] = this.cartService.get();
+  getTotalAmount(orders) {
+    return orders.reduce((sum, order) => {
+      return sum + order.amount
+    }, 0);
+  }
+
+}
